c90deb20e23056dbb5a01219198bb6ce473b2496
Luchohak <a00958934@gmail.com>
2024-02-24 15:53:16 -0600

    Starting setup for mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
+MongoClient.connect(
+  "mongodb+srv://admin:admin123 @cluster0.ibi4wvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
+  .then((result) => console.log("Connected!"))
+  .catch((err) => console.log(err));
