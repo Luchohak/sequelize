762b63f964f648f6a62e70f1d2ef26e4ba637557
Luchohak <a00958934@gmail.com>
2024-02-26 16:08:06 -0600

    Establish connection with mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,15 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect(
-  "mongodb+srv://admin:admin123 @cluster0.ibi4wvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-)
-  .then((result) => console.log("Connected!"))
-  .catch((err) => console.log(err));
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://admin:admin123@cluster0.ibi4wvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = mongoConnect;
